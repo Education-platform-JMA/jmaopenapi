7cd1e1e189cb6714e441b324e254fc56654f7933
fapus <m.cherevko@andersenlab.com>
2021-09-16 09:49:22 +0300

    initial commit

---
+++ src/main/java/ru/jma/jmaopenapi/JmaOpenApiApplication.java
@@ -0,0 +1,13 @@
+package ru.jma.jmaopenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmaOpenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmaOpenApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/jma/jmaopenapi/JmaOpenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.jma.jmaopenapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmaOpenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
